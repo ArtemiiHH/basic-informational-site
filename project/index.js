9106f6c567bf7c8c6eec54882268b2d2e8b14be9
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-02-22 19:13:15 +0200

    Install express

--- project/index.js
+++ project/index.js
@@ -1,43 +1,47 @@
-// Import Node tools
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
+// // Import Node tools
+// const http = require("http");
+// const fs = require("fs");
+// const path = require("path");
 
-// Create server
-const server = http.createServer((req, res) => {
-  // Store name of HTML file
-  let filePath = "";
+// // Create server
+// const server = http.createServer((req, res) => {
+//   // Store name of HTML file
+//   let filePath = "";
 
-  // Routing logic
-  if (req.url === "/") {
-    filePath = "index.html";
-  } else if (req.url === "/about") {
-    filePath = "about.html";
-  } else if (req.url === "/contact-me") {
-    filePath = "contact-me.html";
-  } else {
-    filePath = "404.html";
-  }
+//   // Routing logic
+//   if (req.url === "/") {
+//     filePath = "index.html";
+//   } else if (req.url === "/about") {
+//     filePath = "about.html";
+//   } else if (req.url === "/contact-me") {
+//     filePath = "contact-me.html";
+//   } else {
+//     filePath = "404.html";
+//   }
 
-  // Read file and send to browser
-  fs.readFile(path.join(__dirname, filePath), (err, data) => {
-    // If reading fails send error
-    if (err) {
-      res.writeHead(500, { "Content-Type": "text/html" });
-      res.end("Server error");
-      return;
-    }
+//   // Read file and send to browser
+//   fs.readFile(path.join(__dirname, filePath), (err, data) => {
+//     // If reading fails send error
+//     if (err) {
+//       res.writeHead(500, { "Content-Type": "text/html" });
+//       res.end("Server error");
+//       return;
+//     }
 
-    const statusCode = filePath === "404.html" ? 404 : 200;
+//     const statusCode = filePath === "404.html" ? 404 : 200;
 
-    // Send HTML content back
-    res.writeHead(statusCode, { "Content-Type": "text/html" });
-    // End response and sen data back to browser
-    res.end(data);
-  });
-});
+//     // Send HTML content back
+//     res.writeHead(statusCode, { "Content-Type": "text/html" });
+//     // End response and sen data back to browser
+//     res.end(data);
+//   });
+// });
 
-// Start listening on port 8080
-server.listen(8080, () => {
-  console.log("Server running at http://localhost:8080");
-});
+// // Start listening on port 8080
+// server.listen(8080, () => {
+//   console.log("Server running at http://localhost:8080");
+// });
+
+
+
+// Rewrite logic using Express
