c0b97f1c9f205a3d52072c27be29260280f3dd9c
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-02-22 21:07:34 +0200

    Try to rewrite using express

--- project/index.js
+++ project/index.js
@@ -42,6 +42,32 @@
 //   console.log("Server running at http://localhost:8080");
 // });
 
-
-
 // Rewrite logic using Express
+const { error } = require("console");
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "contact-me.html"));
+});
+
+app.get("/404", (req, res) => {
+  res.sendFile(path.join(__dirname, "404.html"));
+});
+
+app.listen(3000, () => {
+  if (error) {
+    throw error;
+  }
+
+  console.log("Server running at http://localhost:3000");
+});
